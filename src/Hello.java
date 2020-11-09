700bab58a68842648ea7102976b60eabd7e21f52
youoftech <1052195188franik@gmail.com>
2020-11-09 17:24:57 +0800

    second commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
